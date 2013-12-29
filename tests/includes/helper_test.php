18587103ff7cf00aa5f3e8f0e1948f9f38d88d90
kimkha <nkimkha@gmail.com>
2013-12-29 08:07:52 +0700

    Add first version of helper_test.php

---
+++ tests/includes/helper_test.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * helper test
+ *
+ * @since 2.1.0
+ *
+ * @package Redaxscript
+ * @category Tests
+ * @author Kim Kha Nguyen
+ */
+
+class Redaxscript_Helper_Test extends PHPUnit_Framework_TestCase
+{
+	public function testDummy()
+	{
+		$this->assertTrue(true);
+	}
+}
+?>
